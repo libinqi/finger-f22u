41fa677c25bbc379dd489837375f9fb73cb68be6
libinqi <libinqi@outlook.com>
2017-08-03 16:46:46 +0800

    优化bmp图片生成代码

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var finger = require('./lib/F22U');
 
 module.exports.open = finger.open;
 module.exports.close = finger.close;
+module.exports.start = finger.start;
+module.exports.pause = finger.pause;
